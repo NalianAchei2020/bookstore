073c694039c1cc8f2de652945d619936cceff0a8
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-16 14:35:46 +0100

    add books

--- src/components/book.js
+++ src/components/book.js
@@ -7,9 +7,21 @@ function Book() {
       <section className="booklist">
         <article>
           <div>
-            <span>Economy</span>
-            <h3>Capital in the Twenty-First Century</h3>
+            <span>Action</span>
+            <h3>The Hunger Games</h3>
             <span>Suzanne Collins</span>
+            <br />
+            <span>24% Completed</span>
+            <br />
+            <button type="button">Delete</button>
+          </div>
+          <br />
+          <div>
+            <span>Science Fiction</span>
+            <h3>Dune</h3>
+            <span>Suzanne Collins</span>
+            <br />
+            <span>50% Completed</span>
             <br />
             <button type="button">Delete</button>
           </div>
@@ -19,13 +31,7 @@ function Book() {
             <h3>Capital in the Twenty-First Century</h3>
             <span>Suzanne Collins</span>
             <br />
-            <button type="button">Delete</button>
-          </div>
-          <br />
-          <div>
-            <span>Economy</span>
-            <h3>Capital in the Twenty-First Century</h3>
-            <span>Suzanne Collins</span>
+            <span>64% Completed</span>
             <br />
             <button type="button">Delete</button>
           </div>
--- src/components/form.js
+++ src/components/form.js
@@ -4,6 +4,7 @@ function Form() {
   return (
     <section>
       <form>
+        <h4>ADD NEW BOOK</h4>
         <input type="text" placeholder="Book Title" />
         <input type="text" placeholder="Author" />
         <button type="button">Add Book</button>
