2c6190d3951d4b0c1dce0083f9147c458ec10c86
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-22 16:46:14 +0100

    add book reducer

--- src/components/redux/books/booksSlice.js
+++ src/components/redux/books/booksSlice.js
@@ -1,1 +1,21 @@
-ex;
+const initialState = {
+  books: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_BOOK':
+      return {
+        books: [...state.books, action.payload],
+      };
+    case 'DELETE_BOOK':
+      return {
+        books: state.books.filter((book) => book.title !== action.payload),
+      };
+
+    default:
+      return {
+        state,
+      };
+  }
+};
