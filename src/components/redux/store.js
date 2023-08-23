94ad55171cf38fed8f3cff7c2ca5160eba88e9f0
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-23 05:43:42 +0100

    configure store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,1 +1,12 @@
-import redux
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
+
+const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
+});
+
+export default store;
