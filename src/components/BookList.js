d9048abd012468176bd834a948d0bdb84d23a811
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-08-23 07:35:49 +0100

    Update BookList.js

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Book from './book';
 
-function BookList({ books, onDelete }) {
+function BookList() {
   return (
     <div>
       <h2>Book List</h2>
-      {books.map((book) => (
-        <Book
-          key={book.id}
-          title={book.title}
-          author={book.author}
-          onDelete={onDelete}
-        />
-      ))}
+      <Book />
     </div>
   );
 }
-BookList.propTypes = {
-  books: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired,
-};
 
 export default BookList;
