324324ccaf4f4eeb9d01a5c407e14adb1b6317c1
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-09-13 01:58:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Header from './components/header';
 import Book from './pages/books';
 import Category from './pages/category';
+import { getBooks} from './redux/books'
 import './App.css';
 
+
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBooks());
+  }, [dispatch]);
+      
   return (
     <div className="App">
       <Router>
